a24c3f3fef901d9e38eaae7abe9e7812a3608c8e
Mathijs V <nagato.bbs@gmail.com>
2026-03-14 15:51:28 +0100

    improvements serv

--- serv/src/bin/client.rs
+++ serv/src/bin/client.rs
@@ -15,20 +15,20 @@ async fn main() -> anyhow::Result<()> {
     config.tls.disable_verify = Some(true);
 
     let bc_name = String::from("file1");
-    let track_name = String::from("client_1");
+    let client_name = String::from("client_1");
+    let track_name = String::from("update");
+    let sync_name = String::from("sync");
+    let own_bc_name = format!("{bc_name}/client/{client_name}");
 
     let read_origin = moq_lite::Origin::produce();
     let publish_origin = moq_lite::Origin::produce();
     let mut client_bc = Broadcast::produce();
     let mut client_track = client_bc.producer.create_track(Track {
-        name: track_name.clone(),
-        priority: 0,
+        name: track_name,
+        priority: 1,
     });
     publish_origin.producer
         .publish_broadcast(bc_name, client_bc.consumer);
-
-    // Register the broadcast with the local origin so the session will announce and
-    // send it to the server.
 
     let _session = moq_native::Client::new(config)?
         // The session will write broadcasts it receives from the server here.
@@ -38,8 +38,7 @@ async fn main() -> anyhow::Result<()> {
         .connect(url)
         .await?;
 
-    println!("Connected. Publishing 'client' broadcast with 'client' track.");
-    println!("Waiting for broadcast 'echo' from server...");
+    println!("Waiting for broadcast from server...");
 
     // Spawn a task that periodically writes a short string into our published track.
     let publish_task = tokio::spawn(async move {
@@ -49,26 +48,27 @@ async fn main() -> anyhow::Result<()> {
             println!("PUBLISH: {msg}");
             client_track.write_frame(bytes::Bytes::from(msg));
             counter += 1;
-            tokio::time::sleep(Duration::from_secs(4)).await;
+            tokio::time::sleep(Duration::from_secs(5)).await;
         }
     });
 
-    // Watch for broadcasts announced by the server and subscribe to the "main" track
-    // of the "echo" broadcast.
+    // Watch for broadcasts announced by the server 
     let mut origin_consumer = read_origin.consumer;
     let mut thread_set: JoinSet<()> = JoinSet::new();
+    let own_bc_name_clone = own_bc_name.clone();
     while let Some((path, broadcast)) = origin_consumer.announced().await {
         println!("broadcast announced: [{}]", path);
         if let Some(broadcast) = broadcast {
             let path_str = path.to_string();
-            if path_str == track_name {
+            if path_str == own_bc_name_clone {
                 continue
             }
+
+            let sync_t_name = sync_name.clone();
             thread_set.spawn(async move {
-                let mut track_consumer = broadcast.subscribe_track(&moq_lite::Track::new("main"));
-
-                println!("Subscribed to track 'main' on broadcast '{path_str}'");
-
+                let mut track_consumer = broadcast.subscribe_track(&moq_lite::Track::new(&sync_t_name));
+                println!("Subscribed to track on broadcast:[{path_str}], track_name:[{}]", track_consumer.info.name);
+                
                 let mut count = 0usize;
                 while let Ok(Some(mut group)) = track_consumer.next_group().await {
                     if count > 10 {
@@ -77,7 +77,7 @@ async fn main() -> anyhow::Result<()> {
                     while let Ok(Some(frame)) = group.read_frame().await {
                         if let Ok(text) = String::from_utf8(frame.to_vec()) {
                             count += 1;
-                            println!("[{path_str}/main] #{count}: {text}");
+                            println!("[{path_str}/{sync_t_name}] #{count}: {text}");
                         }
                     }
                 }
--- serv/src/main.rs
+++ serv/src/main.rs
@@ -3,20 +3,20 @@ use moq_native::ServerConfig;
 use std::{sync::Arc, time::Duration};
 use tokio::{sync::RwLock, task::JoinSet};
 
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    moq_native::Log::new(tracing::Level::DEBUG).init();
+    //moq_native::Log::new(tracing::Level::DEBUG).init();
 
     let mut config = ServerConfig::default();
-    let files = vec!("file1");
-
+    let files = vec!("file1", "file2");
 
     config.bind = Some("127.0.0.1:4443".parse()?);
     config.tls.generate = vec!["localhost".to_string(), "127.0.0.1".to_string()];
     // publish to clients (echo)
     let publish_origin = Origin::produce();
     // consume from clients
-    let mut consume_origin = Origin::produce();
+    let consume_origin = Origin::produce();
     let mut server = moq_native::Server::new(config)?
         .with_publish(publish_origin.consumer.clone())
         .with_consume(consume_origin.producer.clone());
@@ -60,46 +60,51 @@ async fn run_file(file_name: &str, publish_origin : OriginProducer, mut consume_
     let mut bc = Broadcast::produce();
     // main track to push
     let main_t = bc.producer.create_track(moq_lite::Track {
-        name: format!("{file_name}/sync"),
+        name: "sync".to_string(),
         priority: 0,
     });
     let pushed_string = Arc::new(RwLock::new(format!("moq echo for {file_name}")));
-
-    let main_track = Arc::new(RwLock::new(main_t));
+    let sync_track = Arc::new(RwLock::new(main_t));
 
     publish_origin.publish_broadcast(file_name, bc.consumer);
 
     // sends the full string once per few seconds
     let text_clone = pushed_string.clone();
-    let main_track_clone = main_track.clone();
+    let sync_track_clone = sync_track.clone();
     let name_clone = file_name.to_string();
     let send_thread = tokio::spawn(async move {
         loop {
             let text = text_clone.read().await.clone();
-            let mut main = main_track_clone.write().await;
+            let mut track = sync_track_clone.write().await;
             println!("sync bc [{name_clone}] : [{text}]");
-            main.write_frame(bytes::Bytes::from(text));
+            track.write_frame(bytes::Bytes::from(text));
             tokio::time::sleep(Duration::from_secs(3)).await;
         }
     });
 
+    // handle connecting clients and merge their changes
     let push_str_clone = pushed_string.clone();
     let name_clone = file_name.to_string();
     let receive_thread = tokio::spawn(async move {
-        println!("START LISTEN FOR UPDATE TRACKS IN {name_clone}");
+        println!("START LISTEN FOR UPDATE BCs IN {name_clone}");
         while let Some(announced) = consume_origin.announced().await {
             println!("\nANNOUNCED [{}]\n", announced.0);
             // ignore bc that aren't directed at this file
             // maybe it messes with other threads?
-            if !announced.0.to_string().starts_with(&name_clone) {
+            if !announced.0.to_string().starts_with(&format!("{name_clone}/client/")) {
                 println!("ANNOUNCE SKIP [{}] by [{}]", announced.0, name_clone);
                 continue
             }
-            let bc = announced.1.expect("no broadcast in announced..?");
+            if announced.1.is_none() {
+                println!("CLOSED");
+                continue
+            }
+            let bc = announced.1.unwrap();
 
+            // find update track of client to read changes from
             let push_str_inner_clone = push_str_clone.clone();
             let handle_bc = tokio::spawn(async move {
-                let track_name = String::from("client_1");
+                let track_name = String::from("update");
                 let track = Track {
                     name: track_name.clone(),
                     priority: 0,
@@ -108,7 +113,7 @@ async fn run_file(file_name: &str, publish_origin : OriginProducer, mut consume_
                 while let Ok(Some(mut group)) = client_track.next_group().await {
                     while let Ok(Some(frame)) = group.read_frame().await {
                         if let Ok(text) = String::from_utf8(frame.to_vec()) {
-                            println!("Update: [{}]", text);
+                            println!("Update text over bc [{}]: [{}]", announced.0, text);
                             let mut ps = push_str_inner_clone.write().await;
                             *ps = text;
                         }
